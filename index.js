3e1f9e32d3e3c66728ff220cb00b79a19c229a6c
xarita <128380509+candemir22@users.noreply.github.com>
2025-12-12 08:12:22 +0400

    Update index.js

--- index.js
+++ index.js
@@ -5,6 +5,9 @@ async function initMap() {
     // Get the gmp-map element.
     const mapElement = document.querySelector("gmp-map");
     const map = new google.maps.Map(document.getElementById("map"), {    zoom: 18,    center: { lat: 37.214931, lng: 28.363885 },    mapTypeId: "terrain",  });
+    
+    
+    
     // Get the inner map.
     const innerMap = mapElement.innerMap;
     // Get the feature layer.
@@ -26,3 +29,4 @@ async function initMap() {
 }
 
 initMap();
+

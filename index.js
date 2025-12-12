787a867af74e9258648217e2dd6f21af6c802d1c
xarita <128380509+candemir22@users.noreply.github.com>
2025-12-13 02:55:23 +0400

    Update index.js

--- index.js
+++ index.js
@@ -1,19 +1,15 @@
-<html>
+function initMap() {
+  const m = document.querySelector("gmp-advanced-marker");
+  
+  m.gmpDraggable = true;
 
-  <head>
-    <script async src="https://maps.googleapis.com/maps/api/js?callback=initMap&libraries=maps,marker&v=beta">
-    </script>
-
-  </head>
-
-  <body>
-
-    <div class="map-container">
-      <gmp-map center="42.46598273118431,42.3835740844457525" zoom="12" map-id="DEMO_MAP_ID">
-        <gmp-advanced-marker gmpDraggable="true" position="42.46598273118431,42.3835740844457525" title="Resto" gmpDraggable></gmp-advanced-marker>
-      </gmp-map>
-    </div>
-
-  </body>
-
-</html>
+  // returns void
+  m.addEventListener("gmp-click", (evt) => {
+    console.log('addEventListener gmp-click event', evt);
+  });
+    
+   // returns void
+  m.addListener("dragend", (evt) => {
+    console.log('dragend evt', evt);
+  });
+}

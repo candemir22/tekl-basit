24244af41225cce371ee78a1fd734da9dc38d68a
xarita <128380509+candemir22@users.noreply.github.com>
2025-12-13 02:32:19 +0400

    Update index.js

--- index.js
+++ index.js
@@ -4,7 +4,9 @@ async function initMap() {
     await google.maps.importLibrary("maps");
     // Get the gmp-map element.
     //const mapElement = document.querySelector("gmp-map");
-    const mapElement = new google.maps.Map(document.getElementById("gmp-map"), {    zoom: 18,    center: { lat: 37.214931, lng: 28.363885 },    mapTypeId: "terrain",  });
+
+    
+    
     // Get the inner map.
     const innerMap = mapElement.innerMap;
     // Get the feature layer.
@@ -17,6 +19,13 @@ async function initMap() {
         fillColor: '#810FCB',
         fillOpacity: 0.5
     };
+
+    <gmp-map center="42,42" zoom="4" map-id="DEMO_MAP_ID" style="height: 400px">
+  <gmp-advanced-marker position="42.4220656,42.0840897" title="Mountain View, CA"></gmp-advanced-marker>
+  <gmp-advanced-marker position="42.648994,42.3503845" title="Seattle, WA"></gmp-advanced-marker>
+</gmp-map>
+
+    
     // Apply the style to a single boundary.
     featureLayer.style = (options) => {
         if (options.feature.placeId == 'ChIJ0zQtYiWsVHkRk8lRoB1RNPo') { // Hana, HI
@@ -26,4 +35,5 @@ async function initMap() {
 }
 
 initMap();
+
 

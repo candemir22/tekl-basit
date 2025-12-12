df25675c703fa26ec94718b737ab6bfb923d1b65
xarita <128380509+candemir22@users.noreply.github.com>
2025-12-13 02:54:31 +0400

    Update index.js

--- index.js
+++ index.js
@@ -1,39 +1,19 @@
-let featureLayer;
-async function initMap() {
-    // Request needed libraries.
-    await google.maps.importLibrary("maps");
-    // Get the gmp-map element.
-    //const mapElement = document.querySelector("gmp-map");
+<html>
 
-    
-    
-    // Get the inner map.
-    const innerMap = mapElement.innerMap;
-    // Get the feature layer.
-    featureLayer = innerMap.getFeatureLayer(google.maps.FeatureType.LOCALITY);
-    // Define a style with purple fill and border.
-    const featureStyleOptions = {
-        strokeColor: '#810FCB',
-        strokeOpacity: 1.0,
-        strokeWeight: 3.0,
-        fillColor: '#810FCB',
-        fillOpacity: 0.5
-    };
+  <head>
+    <script async src="https://maps.googleapis.com/maps/api/js?callback=initMap&libraries=maps,marker&v=beta">
+    </script>
 
-    <gmp-map center="42,42" zoom="4" map-id="DEMO_MAP_ID" style="height: 400px">
-  <gmp-advanced-marker position="42.4220656,42.0840897" title="Mountain View, CA"></gmp-advanced-marker>
-  <gmp-advanced-marker position="42.648994,42.3503845" title="Seattle, WA"></gmp-advanced-marker>
-</gmp-map>
+  </head>
 
-    
-    // Apply the style to a single boundary.
-    featureLayer.style = (options) => {
-        if (options.feature.placeId == 'ChIJ0zQtYiWsVHkRk8lRoB1RNPo') { // Hana, HI
-            return featureStyleOptions;
-        }
-    };
-}
+  <body>
 
-initMap();
+    <div class="map-container">
+      <gmp-map center="42.46598273118431,42.3835740844457525" zoom="12" map-id="DEMO_MAP_ID">
+        <gmp-advanced-marker gmpDraggable="true" position="42.46598273118431,42.3835740844457525" title="Resto" gmpDraggable></gmp-advanced-marker>
+      </gmp-map>
+    </div>
 
+  </body>
 
+</html>
